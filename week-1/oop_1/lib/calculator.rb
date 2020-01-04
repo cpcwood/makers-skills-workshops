66531bff12117a2d01cc08cf1f0ad6cf264a73a5
cpcwood <cpcwood@outlook.com>
2020-01-04 18:40:52 +0000

    Week 1 - OOP1 completed

--- week-1/oop_1/lib/calculator.rb
+++ week-1/oop_1/lib/calculator.rb
@@ -1,11 +1,17 @@
-def add(number_1, number_2)
-  number_1 + number_2
+class Calculator
+  #Understands two integers and performs mathematical operation on them
+  def add(number_1, number_2)
+    number_1 + number_2
+  end
+
+  def subtract(number_1, number_2)
+    number_1 - number_2
+  end
 end
 
-def subtract(number_1, number_2)
-  number_1 - number_2
-end
-
-def print_answer(answer)
-  "The Answer is: #{ answer }"
+class Print
+  #Understands string and prints in pretty format
+  def answer(answer)
+    "The Answer is: #{ answer }"
+  end
 end
